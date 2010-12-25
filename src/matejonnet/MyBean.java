ceffaaa62f16e9afed3ebf1d653cb2dd1efcc977
matej <matejonnet@gmail.com>
2010-12-25 17:09:21 +0100

    initial demo commit

---
+++ src/matejonnet/MyBean.java
@@ -0,0 +1,32 @@
+package matejonnet;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
+
+
+@Named
+@RequestScoped
+@ManagedBean
+public class MyBean implements Serializable {
+
+	private static final long serialVersionUID = 1926695191451533895L;
+
+	private String message;
+
+	public MyBean() {}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public String getTime() {
+		return System.currentTimeMillis() + "";
+	}
+}
